9d884d513ff82ecb77ee0118c53c0c7ca952cfbc
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-07-22 23:14:08 +0300

    add data base

--- controller/courses.controller.js
+++ controller/courses.controller.js
@@ -1,47 +1,76 @@
 import {validationResult } from 'express-validator'
 
-import { courses } from '../data/courses.js';
+// import { courses } from '../data/courses.js';
+import course from '../model/course.model.js'
 
-const getAllCourses = (req , res) => {
+const getAllCourses = async (req , res) => {
+    const courses = await course.find()
     res.json(courses)
 }
 
-const getCourse = (req , res) => {
-    const id = +req.params.id
-    const course = courses.find((course) => course.id === id)
-    if(!course){
-        return res.status(404).json({msg : 'Not Found' })
+const getCourse = async (req , res) => {
+    // const id = +req.params.id
+    // const course = courses.find((course) => course.id === id)
+
+    // const course = await course.findById(req.params.id)
+
+    // if(!course){
+    //     return res.status(404).json({msg : 'Not Found' })
+    // }
+    // res.json(course)
+    try{ const courseData = await course.findById(req.params.id)
+
+        if (!courseData) {
+            return res.status(404).json({ msg: 'Not Found' })
+        }
+
+       return res.json(courseData)
+    } catch(err) {
+        return res.status(400).json({ msg: 'Invalide opject id' })
     }
-    res.json(course)
+       
 }
 
-const addCourse = (req , res) => {
+const addCourse = async (req , res) => {
         // console.log(req.body)
         const erorres = validationResult(req)
         if(!erorres.isEmpty()){
             return res.status(400).json(erorres.array())
         }
         // console.log('erorres ' , erorres)
-        courses.push({id : courses.length + 1 , ...req.body})
-        res.status(201).json(courses)
+        // courses.push({id : courses.length + 1 , ...req.body})
+
+        const newCourse = new course(req.body)
+        await newCourse.save()
+        res.status(201).json(newCourse)
 }
 
-const updateCourse =  (req , res) => {
-    const id = +req.params.id
-    let course = courses.find((course) => course.id === id)
-    if(!course){
-        return res.status(404).json({msg : 'Not Found' })
+const updateCourse = async (req , res) => {
+    // const id = +req.params.id
+    // let course = courses.find((course) => course.id === id)
+    const courseid = req.params.id
+    try{
+         const updatedCourses = await course.updateOne({_id : courseid } , {$set: {...req.body}} )
+         return res.status(200).json(updatedCourses)
+    } catch(err) {
+        return res.status(400).json({error : err})
     }
+   
+
+    // if(!course){
+    //     return res.status(404).json({msg : 'Not Found' })
+    // }
+    // course = {...course , ...req.body}
     
-    course = {...course , ...req.body}
-    res.status(200).json(course)
 }
 
-const daleteCourse =  (req , res) => {
-    const id = +req.params.id
-    courses = courses.filter((course) => course.id !== id)
+const daleteCourse = async (req , res) => {
+    // const id = +req.params.id
+    // courses = courses.filter((course) => course.id !== id)
+
+    const data = await course.deleteOne({_id : req.params.id})
     
-     res.status(200).json({success : true})
+     res.status(200).json({success : true , msg : data})
    
 }
 
--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 import { error } from 'console';
 import express from 'express';
+import mongoose from 'mongoose';
+
+const url = "mongodb+srv://godzela1242006:Medo1242006@learn-mongo-db.zmrfxdx.mongodb.net/Medo?retryWrites=true&w=majority&appName=learn-mongo-db"
+mongoose.connect(url).then(() => {
+    console.log('Mongodb connected success')
+})
+
 const app = express()
 const port = 3000
 
